a46e218ae9e3a1ad13ca1e48647d1ade08e3a0ec
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-06-20 18:06:03 +0100

    Added a function to declare who won

--- script.js
+++ script.js
@@ -1,12 +1,78 @@
 // JAVASCRIPT CODE FOR THE GAME 
 
-function randomDiceImage() {
-    const number = 6;
-    const randomIndex = Math.floor(Math.random() * number);
-    const diceImages = ["./images/dice1.png", "./images/dice2.png", "./images/dice3.png",
-                        "./images/dice4.png", "./images/dice5.png", "./images/dice6.png"];
-    return diceImages[randomIndex];
+// JAVASCRIPT CODE
+
+// Create function to randomly select  player 1 images 
+function randomLeftImage() {
+    // Generate a random number between 0 & 6
+    const number = 6; 
+    const randomIndex1 = Math.floor(Math.random() * number); 
+
+    // Create a list for the images
+    const leftIMage = ["./images/dice1.png","./images/dice2.png","./images/dice3.png",
+        "./images/dice4.png","./images/dice5.png","./images/dice6.png"
+     ];
+
+    //  Create a random selection of the images
+    return leftIMage[randomIndex1]
 }
 
-document.querySelector(".img1").setAttribute("src", randomDiceImage());
-document.querySelector(".img2").setAttribute("src", randomDiceImage());
+
+// Set the src attribute to the leftImage
+// Select the image element
+const player1Image = document.querySelector(".img1");
+
+// Set its source attribute
+player1Image.setAttribute("src", randomLeftImage()); 
+
+
+// Create a function to randomly select player 2 images
+function randomRightImage() {
+    // Generate a random number between 0 & 6
+    const number = 6; 
+    const randomIndex2 = Math.floor(Math.random() * number); 
+
+    // Create a list for the images
+    const rightIMage = ["./images/dice1.png","./images/dice2.png","./images/dice3.png",
+        "./images/dice4.png","./images/dice5.png","./images/dice6.png"
+     ];
+
+    //  Create a random selection of the images
+    return rightIMage[randomIndex2];
+}
+
+
+
+// Set the src attribute to the leftImage
+// Select the image element
+const player2Image = document.querySelector(".img2");
+
+// Set its source attribute
+player2Image.setAttribute("src", randomRightImage()); 
+
+
+// Get the image the src attribute is displaying and save in a a reassignable variable 
+let leftDispayImage = document.querySelector(".img1").getAttribute("src"); 
+let rightDisplayImage = document.querySelector(".img2").getAttribute("src");
+
+// Declare who won, or if is a tie. 
+function whoWon() {
+    let message; 
+    const heading = document.querySelector("h1"); 
+
+    if (leftDispayImage === "./images/dice6.png" && rightDisplayImage === "./images/dice6.png") {
+        heading.innerHTML = ""; 
+        message = heading.innerHTML = "Draw"; 
+    } else if (leftDispayImage === "./images/dice6.png") {
+        heading.innerHTML = ""; 
+        message = heading.innerHTML = "Player 1 Won !"; 
+    } else if (rightDisplayImage === "./images/dice6.png") {
+        heading.innerHTML = ""; 
+        message = heading.innerHTML = "Player 2 Won !"; 
+    } else {
+        heading.innerHTML = "Refresh Me"; 
+    }
+    return message; 
+}
+
+whoWon(); 
