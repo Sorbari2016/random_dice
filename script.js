242b04bc6a1e7388a7471f3cfaae78d49fccc7cc
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-06-20 16:27:11 +0100

    Refactored the randomImages functions

--- script.js
+++ script.js
@@ -1,48 +1,12 @@
-// JAVASCRIPT CODE
+// JAVASCRIPT CODE FOR THE GAME 
 
-// Create function to randomly select  player 1 images 
-function randomLeftImage() {
-    // Generate a random number between 0 & 6
-    const number = 6; 
-    const randomIndex1 = Math.floor(Math.random() * number); 
-
-    // Create a list for the images
-    const leftIMage = ["./images/dice1.png","./images/dice2.png","./images/dice3.png",
-        "./images/dice4.png","./images/dice5.png","./images/dice6.png"
-     ];
-
-    //  Create a random selection of the images
-    return leftIMage[randomIndex1]
+function randomDiceImage() {
+    const number = 6;
+    const randomIndex = Math.floor(Math.random() * number);
+    const diceImages = ["./images/dice1.png", "./images/dice2.png", "./images/dice3.png",
+                        "./images/dice4.png", "./images/dice5.png", "./images/dice6.png"];
+    return diceImages[randomIndex];
 }
 
-
-// Set the src attribute to the leftImage
-// Select the image element
-const player1Image = document.querySelector(".img1");
-
-// Set its source attribute
-player1Image.setAttribute("src", randomLeftImage()); 
-
-
-// Create a function to randomly select player 2 images
-function randomRightImage() {
-    // Generate a random number between 0 & 6
-    const number = 6; 
-    const randomIndex2 = Math.floor(Math.random() * number); 
-
-    // Create a list for the images
-    const rightIMage = ["./images/dice1.png","./images/dice2.png","./images/dice3.png",
-        "./images/dice4.png","./images/dice5.png","./images/dice6.png"
-     ];
-
-    //  Create a random selection of the images
-    return rightIMage[randomIndex2];
-}
-
-
-// Set the src attribute to the leftImage
-// Select the image element
-const player2Image = document.querySelector(".img2");
-
-// Set its source attribute
-player2Image.setAttribute("src", randomRightImage()); 
+document.querySelector(".img1").setAttribute("src", randomDiceImage());
+document.querySelector(".img2").setAttribute("src", randomDiceImage());
