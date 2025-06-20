8c9c8969ac66dfcde3d8428e18c13b08816ea4b6
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2025-06-20 18:13:41 +0100

    Refactored the code to make it cleaner and shorter

--- script.js
+++ script.js
@@ -1,54 +1,26 @@
 // JAVASCRIPT CODE FOR THE GAME 
 
-// JAVASCRIPT CODE
+// Create function to randomly select  random images.
 
-// Create function to randomly select  player 1 images 
-function randomLeftImage() {
+function randomImages() {
     // Generate a random number between 0 & 6
     const number = 6; 
-    const randomIndex1 = Math.floor(Math.random() * number); 
+    const randomIndex = Math.floor(Math.random() * number); 
 
     // Create a list for the images
-    const leftIMage = ["./images/dice1.png","./images/dice2.png","./images/dice3.png",
+    const playerImages = ["./images/dice1.png","./images/dice2.png","./images/dice3.png",
         "./images/dice4.png","./images/dice5.png","./images/dice6.png"
      ];
 
     //  Create a random selection of the images
-    return leftIMage[randomIndex1]
+    return playerImages[randomIndex]; 
 }
 
 
-// Set the src attribute to the leftImage
-// Select the image element
-const player1Image = document.querySelector(".img1");
-
-// Set its source attribute
-player1Image.setAttribute("src", randomLeftImage()); 
-
-
-// Create a function to randomly select player 2 images
-function randomRightImage() {
-    // Generate a random number between 0 & 6
-    const number = 6; 
-    const randomIndex2 = Math.floor(Math.random() * number); 
-
-    // Create a list for the images
-    const rightIMage = ["./images/dice1.png","./images/dice2.png","./images/dice3.png",
-        "./images/dice4.png","./images/dice5.png","./images/dice6.png"
-     ];
-
-    //  Create a random selection of the images
-    return rightIMage[randomIndex2];
-}
-
-
-
-// Set the src attribute to the leftImage
-// Select the image element
-const player2Image = document.querySelector(".img2");
-
-// Set its source attribute
-player2Image.setAttribute("src", randomRightImage()); 
+// Set the src attribute for both players.
+// Select the image element, & set the attribute
+document.querySelector(".img1").setAttribute("src", randomImages()); 
+document.querySelector(".img2").setAttribute("src", randomImages());
 
 
 // Get the image the src attribute is displaying and save in a a reassignable variable 
